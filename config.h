320a4940378eef3062dfeca8cb4761a151eed53d
VID-PRO <info@vid-pro.de>
2026-06-07 20:45:41 +0200

    bump v1.1.1

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 #include <Preferences.h>
 
-#define FIRMWARE_VERSION "1.1.0"
+#define FIRMWARE_VERSION "1.1.1"
 #define OTA_REPO "VID-PRO/BambuTagger-AMS-C"
 
 #define NUM_SLOTS 4
